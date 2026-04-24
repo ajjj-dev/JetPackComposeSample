df966806942c3fff6619841222781700dcc6a0fc
aj <ajjj.dev@gmail.com>
2026-04-25 01:42:07 +0530

    Implemented OnClick listener for button clicks

--- app/src/main/java/com/aj/jpcsample/MainActivity.kt
+++ app/src/main/java/com/aj/jpcsample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.aj.jpcsample
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,37 +11,38 @@ import androidx.core.view.WindowInsetsCompat
 import com.aj.jpcsample.jpc.HappyBirthdayActivity
 import com.aj.jpcsample.jpc.JpcTutorialActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        // Initialize Views and set OnCLik listeners
         val btnHappyBirthdayActivity = findViewById<Button>(R.id.btn_happy_birthday_activity)
         val btnJpcTutorialActivity = findViewById<Button>(R.id.btn_jpc_tutorial_activity)
-
-        btnHappyBirthdayActivity.setOnClickListener {
-            startActivity(
-                Intent(
-                    this,
-                    HappyBirthdayActivity::class.java
-                )
-            )
-        }
-
-        btnJpcTutorialActivity.setOnClickListener {
-            startActivity(
-                Intent(
-                    this,
-                    JpcTutorialActivity::class.java
-                )
-            )
-        }
+        btnHappyBirthdayActivity.setOnClickListener(this)
+        btnJpcTutorialActivity.setOnClickListener(this)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_happy_birthday_activity -> {
+                navigateTo(HappyBirthdayActivity::class.java)
+            }
+
+            R.id.btn_jpc_tutorial_activity -> {
+                navigateTo(JpcTutorialActivity::class.java)
+            }
+        }
+    }
+
+    private fun navigateTo(clazz: Class<*>) {
+        startActivity(Intent(this, clazz))
     }
 }
